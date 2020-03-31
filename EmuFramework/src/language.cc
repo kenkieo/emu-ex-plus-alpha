6dd0719723762f43360dbaead024e1a37a9fcd62
kenkieo <kenkieo@163.com>
2020-03-31 17:32:03 +0800

    change use language_zh to replace language

--- EmuFramework/src/language.cc
+++ EmuFramework/src/language.cc
@@ -2,8 +2,8 @@
 #include <string>
 
 char *get_local_language(char *key) {
-    if (language.count(key) > 0) {
-        return const_cast<char *>(language[key].c_str());
+    if (language_zh.count(key) > 0) {
+        return const_cast<char *>(language_zh[key].c_str());
     } else {
         return key;
     }
